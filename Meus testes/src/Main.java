21dc5a4f50ae119dbde581f669b0c96c724d7626
Syllos <joaohenrique.braga@outlook.com>
2025-02-13 20:33:16 -0300

    Primeiros segundos passos em java

---
+++ Meus testes/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[]  args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero1 = getNumero(scanner);
+        int numero2 = getNumero(scanner);
+        String sinal = getSinal(scanner);
+
+        if (sinal.equals("/")) {
+            if (numero2 == 0){
+                System.out.println("Digite um numero Valido");
+                numero2 = getNumero(scanner);
+            } else {
+                System.out.println("Resultado: " + (numero1 / numero2));
+            }
+        }
+        else if (sinal.equals("*")) {
+            System.out.println("Resultado: " + (numero1 * numero2));
+        }
+
+        else if (sinal.equals("-")) {
+            System.out.println("Resultado: " + (numero1 - numero2));
+        }
+
+        else if (sinal.equals("+")){
+            System.out.println("Resultado: " + ((numero1 + numero2)));
+        }
+
+        else {
+            System.out.println("Sinal INVALIDO");
+        }
+    scanner.close();
+    }
+
+    public static int getNumero (Scanner scanner) {
+        System.out.println("Digite um numero");
+        return scanner.nextInt();
+    }
+
+    public static String getSinal (Scanner scanner) {
+        System.out.println("Digite o sinal (/, +, -, *)");
+        return scanner.next();
+    }
+}
